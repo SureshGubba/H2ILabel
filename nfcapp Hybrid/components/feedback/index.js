5577a5ae60238f3e3ec622eb4cc54d00e6b7bd17
Suresh Gubba <sureshgubba@gmail.com>
2015-09-21 10:08:12 +0000

    Changes from Screen Builder

--- nfcapp Hybrid/components/feedback/index.js
+++ nfcapp Hybrid/components/feedback/index.js
@@ -9,6 +9,7 @@ app.feedback = kendo.observable({
 
 function LoadFeedbacks()
 {
+    
     console.log('nfc');
     var WebAPIURL = "http://schneidernfcservices.cloudapp.net/api";
 	var AuthKey = "1cede1a2-e7d4-44f4-ab39-75c646c88b71";
