94cfe0b37cd8f1217bee45f1a817384b11d270bf
Suresh Gubba <sureshgubba@gmail.com>
2015-09-22 09:07:10 +0000

    Changes from Screen Builder

--- nfcapp Hybrid/components/feedback/index.js
+++ nfcapp Hybrid/components/feedback/index.js
@@ -1,32 +1,37 @@
 'use strict';
 
 app.feedback = kendo.observable({
-    onShow: function() {
+    onShow: function () {
         LoadFeedbacks();
     },
-    afterShow: function() {}
+    afterShow: function () {}
 });
 
-function LoadFeedbacks()
-{
-    
+
+function LoadFeedbacks() {
+
     console.log('nfc');
     var WebAPIURL = "http://schneidernfcservices.cloudapp.net/api";
-	var AuthKey = "1cede1a2-e7d4-44f4-ab39-75c646c88b71";
+    var AuthKey = "1cede1a2-e7d4-44f4-ab39-75c646c88b71";
     $.ajax({
-                type: "GET",
-                crossDomain: true,
-                url: WebAPIURL + "/feedback/GetFeedbacks?Authorization=" + AuthKey,
-                dataType: "json",
-                success: function (data) {                  
-				console.log(data);
-                     $("#feedbacks").kendoMobileListView({
-                        dataSource: data,
-                         template: "<div><span style='visibility: hidden;' >#: data.FeedbackID#</span><span>Rating #: data.Rating #</span><span>#: data.Comments # </span><span>#: data.CreatedByUser.EmailID # </span></div></div>"
-                    });
-                },
-                error: function (xhr) {                    
-                    alert(xhr.responseText);
-                }
-            })
+        type: "GET",
+        crossDomain: true,
+        url: WebAPIURL + "/feedback/GetFeedbacks?Authorization=" + AuthKey,
+        dataType: "json",
+        success: function (data) {
+            console.log(data);
+            $("#feedbacks").kendoMobileListView({
+                dataSource: data,
+                template: "<div>\
+                				 <div style='visibility: hidden;' >#: data.FeedbackID#</div>\
+                                 <div>Rating #: data.Rating #</div>\
+                                 <div>#: data.Comments # </div>\
+                                 <div<a href='mailto:#: data.CreatedByUser.EmailID #'>#: data.CreatedByUser.EmailID #</a></div>\
+                         </div>"
+            });
+        },
+        error: function (xhr) {
+            alert(xhr.responseText);
+        }
+    })
 }
