ad774abbeb786c38d14f2f9c76ce7bc86e4d9557
Suresh Gubba <sureshgubba@gmail.com>
2015-09-23 07:40:58 +0000

    Changes made by Suresh Gubba (sureshgubba@gmail.com).

--- nfcapp Hybrid/components/users/index.js
+++ nfcapp Hybrid/components/users/index.js
@@ -1,32 +1,35 @@
 'use strict';
 
 app.users = kendo.observable({
-    onShow: function() {LoadNFCUsers();},
-    afterShow: function() {}
+    onShow: function () {
+        LoadNFCUsers();
+    },
+    afterShow: function () {}
 });
 
 // START_CUSTOM_CODE_users
 
 function LoadNFCUsers() {
-    console.log('nfc');
-    var WebAPIURL = "http://schneidernfcservices.cloudapp.net/api";
-    var AuthKey = "1cede1a2-e7d4-44f4-ab39-75c646c88b71";
-    $.ajax({
-        type: "GET",
-        crossDomain: true,
-        url: WebAPIURL + "/user/GetUsers?Authorization=" + AuthKey,
-        dataType: "json",
-        success: function(data) {
-            console.log(data);
-            $("#users").kendoMobileListView({
-                dataSource: data,
-                template: "<span>#: data.EmailID #</span>"
-            });
-        },
-        error: function(xhr) {
-            alert(xhr.responseText);
+        console.log('nfc');
+        var WebAPIURL = "http://schneidernfcservices.cloudapp.net/api";
+        var AuthKey = "1cede1a2-e7d4-44f4-ab39-75c646c88b71";
+        $.ajax({
+                type: "GET",
+                crossDomain: true,
+                url: WebAPIURL + "/user/GetUsers?Authorization=" + AuthKey,
+                dataType: "json",
+                success: function (data) {
+                    console.log(data);
+                    $("#users").kendoMobileListView({
+                        dataSource: data,
+                        template: "<div> \
+                			<div> #: data.EmailID # </div> \
+                			<div> #: data.Password # </div> </div>"
+                    })
+                    error: function (xhr) {
+                        alert(xhr.responseText);
+                    }
+                })
         }
-    })
-}
 
-// END_CUSTOM_CODE_users
+        // END_CUSTOM_CODE_users
