da63d4bfa28265354899f307097d7a2097fb9e0f
Suresh Gubba <sureshgubba@gmail.com>
2015-09-23 07:33:02 +0000

    Changes made by Suresh Gubba (sureshgubba@gmail.com).

--- nfcapp Hybrid/components/feedback/index.js
+++ nfcapp Hybrid/components/feedback/index.js
@@ -1,9 +1,36 @@
 'use strict';
 
 app.feedback = kendo.observable({
-    onShow: function() {},
+    onShow: function() {
+        LoadFeedbacks();
+    },
     afterShow: function() {}
 });
 
 // START_CUSTOM_CODE_feedback
+function LoadFeedbacks() {
+    console.log('nfc');
+    var WebAPIURL = "http://schneidernfcservices.cloudapp.net/api";
+    var AuthKey = "1cede1a2-e7d4-44f4-ab39-75c646c88b71";
+    $.ajax({
+        type: "GET",
+        crossDomain: true,
+        url: WebAPIURL + "/Feedback/GetFeedBacks?Authorization=" + AuthKey,
+        dataType: "json",
+        success: function(data) {
+            console.log(data);
+            $("#feedbacks").kendoMobileListView({
+                dataSource: data,
+                template: "<div> \
+							<div>#: data.Comments #</div> \
+							<div>Rating :#: data.Rating #</div> \
+                			<div>#: data.CreatedByUser.EmailID # #: data.CreatedByUser.CompanyName # #: data.CreatedByUser.Country #</div> \
+                		   </div>"
+            });
+        },
+        error: function(xhr) {
+            alert(xhr.responseText);
+        }
+    })
+}
 // END_CUSTOM_CODE_feedback
