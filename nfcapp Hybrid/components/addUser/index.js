3b5508bfeb4196cdcfc8e8f605804376f24e92c3
Suresh Gubba <sureshgubba@gmail.com>
2015-09-25 11:07:29 +0000

    Changes from Screen Builder

--- nfcapp Hybrid/components/addUser/index.js
+++ nfcapp Hybrid/components/addUser/index.js
@@ -10,15 +10,14 @@ app.addUser = kendo.observable({
 (function(parent) {
     var addUserModel = kendo.observable({
         fields: {
+            platform: '',
             application: '',
             country: '',
             companyName: '',
             password1: '',
             email1: '',
         },
-        submit: function() {
-            AddNewUser(addUserModel.fields.email1,addUserModel.fields.password1,addUserModel.fields.companyName,addUserModel.fields.country,addUserModel.fields.application);
-        },
+        submit: function() {},
         cancel: function() {}
     });
 
@@ -27,29 +26,28 @@ app.addUser = kendo.observable({
 
 // START_CUSTOM_CODE_addUserModel
 
-function AddNewUser(email1,password1,companyName,country,application)
-{
-     var WebAPIURL = "http://schneidernfcservices.cloudapp.net/api";
+function AddNewUser(email1, password1, companyName, country, application) {
+    var WebAPIURL = "http://schneidernfcservices.cloudapp.net/api";
     var AuthKey = "1cede1a2-e7d4-44f4-ab39-75c646c88b71";
     $.ajax({
-                type: "POST",
-                crossDomain: true,
-                url: WebAPIURL + "/User/RegisterUser?Authorization=" + AuthKey,
-                data: {
-                    "EmailID": email1,
-                    "Password": password1,
-                    "Country": country,
-                    "CompanyName": companyName,
-                    "Platform": country,
-                    "ApplicationName": application
-                },
-                success: function (data) {
-                    
-                    app.mobileApp.navigate($('#usersview').attr("href"));
-                },
-                error: function (xhr) {
-                    alert(xhr.responseText);
-                }
-            });
+        type: "POST",
+        crossDomain: true,
+        url: WebAPIURL + "/User/RegisterUser?Authorization=" + AuthKey,
+        data: {
+            "EmailID": email1,
+            "Password": password1,
+            "Country": country,
+            "CompanyName": companyName,
+            "Platform": country,
+            "ApplicationName": application
+        },
+        success: function(data) {
+
+            app.mobileApp.navigate($('#usersview').attr("href"));
+        },
+        error: function(xhr) {
+            alert(xhr.responseText);
+        }
+    });
 }
 // END_CUSTOM_CODE_addUserModel
